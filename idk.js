6a9a4f9222079499d42c47b0e2058b299977dc42
Maryna Grytsenko <marynagrytsenko08@gmail.com>
2026-03-23 15:17:46 +0200

    new code

---
+++ idk.js
@@ -0,0 +1,12 @@
+function memoize(fn) {
+  const cache = new Map();
+
+  return function (...args) {
+    const key = JSON.stringify(args);
+
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+
+  };
+}
