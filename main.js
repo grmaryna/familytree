caa59084bbb51dc3a18c13af5838605b040de018
Maryna Grytsenko <marynagrytsenko08@gmail.com>
2026-03-29 20:40:49 +0300

    new

---
+++ main.js
@@ -0,0 +1,19 @@
+const createTree = document.getElementById('createTree');
+createTree.addEventListener('click', () => {
+    window.location.href = 'createTree.html';
+})
+
+const treeList = document.getElementById('treeList');
+treeList.addEventListener('click', () => {
+    window.location.href = './treeList.html';
+})
+
+const signIn = document.getElementById('signIn');
+treeList.addEventListener('click', () => {
+    window.location.href = './signIn.html';
+})
+
+const seatings = document.getElementById('seatings');
+treeList.addEventListener('click', () => {
+    window.location.href = './seatings.html';
+})
